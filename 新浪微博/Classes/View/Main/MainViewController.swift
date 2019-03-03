38632d0caf97c657f68eebb0370e057b42f3a5db
lianghuajian <624935495@qq.com>
2019-03-03 10:32:31 +0800

    搭建项目结构以及创建各控制器的模板文件

---
+++ 新浪微博/Classes/View/Main/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  新浪微博
+//
+//  Created by 梁华建 on 2019/3/3.
+//  Copyright © 2019 梁华建. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
