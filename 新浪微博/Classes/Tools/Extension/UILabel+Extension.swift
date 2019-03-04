bdae2a1c3d4a259ca27f89dc318d5fbf8a81c838
lianghuajian <624935495@qq.com>
2019-03-04 13:50:33 +0800

    用Visitor类作为四个主界面的父类，并且一个用户是否登录来判断显示给用户登录界面还是未登录界面

---
+++ 新浪微博/Classes/Tools/Extension/UILabel+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UILabel+Extension.swift
+//  新浪微博
+//
+//  Created by 梁华建 on 2019/3/4.
+//  Copyright © 2019 梁华建. All rights reserved.
+//
+
+import Foundation
---
+++ 新浪微博/Classes/View/Main/Visitor/VisitorView.swift
@@ -0,0 +1,21 @@
+//
+//  VisitorView.swift
+//  新浪微博
+//
+//  Created by 梁华建 on 2019/3/4.
+//  Copyright © 2019 梁华建. All rights reserved.
+//
+
+import UIKit
+
+class VisitorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
