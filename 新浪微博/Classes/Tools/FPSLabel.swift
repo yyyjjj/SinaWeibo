1f68c574afa88c5eb008e49db4c93cec1f8c0f3c
lianghuajian <624935495@qq.com>
2019-04-11 09:36:42 +0800

    撰写发布微博控制器，添加表情键盘的toolBar

---
+++ 新浪微博/Classes/Tools/FPSLabel.swift
@@ -0,0 +1,21 @@
+//
+//  FPSLabel.swift
+//  新浪微博
+//
+//  Created by 梁华建 on 2019/4/6.
+//  Copyright © 2019 梁华建. All rights reserved.
+//
+
+import UIKit
+
+class FPSLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
