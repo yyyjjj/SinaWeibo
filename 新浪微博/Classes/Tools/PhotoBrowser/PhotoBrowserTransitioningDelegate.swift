fc6e3cf2e0c308f50e3e70f628dc2a9d251a1079
lianghuajian <624935495@qq.com>
2019-04-16 17:17:22 +0800

    搭建转场动画的基本步骤

---
+++ 新浪微博/Classes/Tools/PhotoBrowser/PhotoBrowserTransitioningDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoBrowserTransitioningDelegate.swift
+//  新浪微博
+//
+//  Created by 梁华建 on 2019/4/16.
+//  Copyright © 2019 梁华建. All rights reserved.
+//
+
+import UIKit
+
+class PhotoBrowserTransitioningDelegate: UIViewControllerTransitioningDelegate {
+
+}
